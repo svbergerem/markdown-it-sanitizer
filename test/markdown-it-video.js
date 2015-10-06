2cc6e50a6ab298d888ce34f09b58ff47bc926969
Steffen van Bergerem <svbergerem@online.de>
2015-10-06 10:13:37 +0200

    Test markdown-it-video with different sanitizer options

--- test/markdown-it-video.js
+++ test/markdown-it-video.js
@@ -4,7 +4,7 @@ var path = require('path');
 var generate = require('markdown-it-testgen');
 var video = require('markdown-it-video');
 
-describe('markdown-it-video', function () {
+describe('sanitzier + markdown-it-video', function () {
   var md = require('markdown-it')({
     html: true,
     langPrefix: '',
@@ -12,6 +12,25 @@ describe('markdown-it-video', function () {
     linkify: true
   });
 
-  md.use(require('../')).use(video);
-  generate(path.join(__dirname, 'fixtures/vendor/markdown-it-video'), md);
+  md.use(video);
+
+  it('works with default values (both false)', function () {
+    md.use(require('../'));
+    generate(path.join(__dirname, 'fixtures/vendor/markdown-it-video'), md);
+  });
+
+  it('works with removeUnknown as an option', function () {
+    md.use(require('../'), { removeUnbalanced: false, removeUnknown: true });
+    generate(path.join(__dirname, 'fixtures/vendor/markdown-it-video'), md);
+  });
+
+  it('works with removeUnbalanced as an option', function () {
+    md.use(require('../'), { removeUnbalanced: true, removeUnknown: false });
+    generate(path.join(__dirname, 'fixtures/vendor/markdown-it-video'), md);
+  });
+
+  it('works with removeUnknown and removeUnbalanced as options', function () {
+    md.use(require('../'), { removeUnbalanced: true, removeUnknown: true });
+    generate(path.join(__dirname, 'fixtures/vendor/markdown-it-video'), md);
+  });
 });
